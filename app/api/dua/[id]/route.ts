93842f2ffcc56f963076474a262461418d9c622f
Bablu22 <mdbablu22.bablu22@gmail.com>
2024-03-29 17:37:25 +0600

    create api change

--- app/api/dua/[id]/route.ts
+++ app/api/dua/[id]/route.ts
@@ -1,9 +1,8 @@
 import { prisma } from "@/prisma";
-import { NextApiRequest } from "next";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
-  req: NextApiRequest,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const { id } = params;
--- app/duas/[id]/page.tsx
+++ app/duas/[id]/page.tsx
@@ -1,18 +1,22 @@
-import { prisma } from "@/prisma";
 import { notFound } from "next/navigation";
 import DuaDescriptionCard from "../_components/DuaDescriptionCard";
+import axios from "axios";
 
 interface Props {
   params: { id: string };
 }
 
-const DuaPage = async ({ params }: Props) => {
+const fetchDua = async (id: number) => {
   const dua = await fetch(
-    `https://dua-app-eight.vercel.app/api/dua/${Number(params.id)}`
+    `https://dua-app-eight.vercel.app/api/dua/${id}`
   ).then((res) => res.json());
+  return dua;
+};
 
+const DuaPage = async ({ params }: Props) => {
+  const dua = await fetchDua(Number(params.id));
   if (!dua) {
-    notFound();
+    return notFound();
   }
 
   return (
